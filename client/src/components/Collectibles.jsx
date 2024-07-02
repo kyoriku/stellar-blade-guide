3157317c480bc9b0b01ce9753e6d309f81248e23
Austin Graham <austiingraham@gmail.com>
2024-07-02 00:03:57 -0400

    replaced loading spinner with loading skeletons - pretty happy with it so far but could be more fluid

--- client/src/components/Collectibles.jsx
+++ client/src/components/Collectibles.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import MediaDisplay from "./MediaDisplay";
-import LoadingSpinner from './LoadingSpinner';
+import { Skeleton } from "@mui/material";
+import '../styles/Collectibles.css';
 
 const Collectibles = ({ fetchCollectibles, title, id, renderItem }) => {
   const [content, setContent] = useState([]);
@@ -9,6 +10,7 @@ const Collectibles = ({ fetchCollectibles, title, id, renderItem }) => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        setIsLoading(true);
         const data = await fetchCollectibles();
         setContent(data);
       } catch (err) {
@@ -21,26 +23,47 @@ const Collectibles = ({ fetchCollectibles, title, id, renderItem }) => {
     fetchData();
   }, [fetchCollectibles]);
 
+  if (isLoading) {
+    return (
+      <div>
+        <hr id={id}></hr>
+        <h3>▽ {title} Collectibles</h3>
+        <hr className='w-75'></hr>
+        {isLoading && (
+          <div>
+            {[...Array(5)].map((_, index) => (
+              <div key={index}>
+                <Skeleton animation="wave" height={20} width="90%" className="skeleton-title" />
+                <Skeleton animation="wave" height={20} width="60%" className="skeleton-subtitle" />
+                <div className="skeleton-container">
+                  <Skeleton animation="wave" height={212} width={376} variant="rounded" className="skeleton-item" />
+                  <Skeleton animation="wave" height={212} width={376} variant="rounded" className="skeleton-item" />
+                </div>
+                <hr></hr>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+    );
+  }
+
   return (
     <div>
       <hr id={id}></hr>
       <h3>▽ {title} Collectibles</h3>
       <hr className='w-75'></hr>
-      {isLoading ? (
-        <LoadingSpinner />
-      ) : (
-        content.map((item, index) => (
-          renderItem ? renderItem(item, index, content) : (
-            <MediaDisplay
-              key={item.id}
-              title={item.title}
-              text={item.text}
-              images={item.images}
-              showHr={index !== content.length - 1 || item.text}
-            />
-          )
-        ))
-      )}
+      {content.map((item, index) => (
+        renderItem ? renderItem(item, index, content) : (
+          <MediaDisplay
+            key={item.id}
+            title={item.title}
+            text={item.text}
+            images={item.images}
+            showHr={index !== content.length - 1 || item.text}
+          />
+        )
+      ))}
     </div>
   );
 }
