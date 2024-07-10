03ea11303083a658c87a4c0816c3abddaf5d3116
Austin Graham <austiingraham@gmail.com>
2024-07-09 23:09:24 -0400

    added routes for Matrix 11 collectibles

--- server/routes/api/collectibles-routes.js
+++ server/routes/api/collectibles-routes.js
@@ -4,19 +4,21 @@ const eidos7Routes = require('./levels/eidos-7-routes');
 const xionRoutes = require('./levels/xion-routes');
 const wastelandRoutes = require('./levels/wasteland-routes');
 const altessLevoireRoutes = require('./levels/altess-levoire-routes');
-// const matrix11Routes = require('./levels/matrix11-routes');
+const matrix11Routes = require('./levels/matrix-11-routes');
 // const greatDesertRoutes = require('./levels/greatDesert-routes');
 // const abyssLevoireRoutes = require('./levels/abyssLevoire-routes');
 // const eidos9Routes = require('./levels/eidos9-routes');
 // const spire4Routes = require('./levels/spire4-routes');
 
+// Route to get all collectibles
 router.route('/').get(getAllCollectibles);
 
+// Routes to get collectibles from levels
 router.use('/eidos-7', eidos7Routes);
 router.use('/xion', xionRoutes);
 router.use('/wasteland', wastelandRoutes);
 router.use('/altess-levoire', altessLevoireRoutes);
-// router.use('/matrix-11', matrix11Routes);
+router.use('/matrix-11', matrix11Routes);
 // router.use('/great-desert', greatDesertRoutes);
 // router.use('/abyss-levoire', abyssLevoireRoutes);
 // router.use('/eidos-9', eidos9Routes);
