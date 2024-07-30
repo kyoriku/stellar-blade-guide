9634658d59c95c29bf03c456552f259f2aa59993
Austin Graham <austiingraham@gmail.com>
2024-07-30 15:00:00 -0400

    updated ContentText component to handle array and string text formats

--- client/src/components/ContentText.jsx
+++ client/src/components/ContentText.jsx
@@ -1,11 +1,26 @@
 import React from "react";
 
-const ContentText = ({ title, text }) => (
-  <p>
-    <strong>{title}</strong>
-    <span> &#8211; </span>
-    {text}
-  </p>
-);
+const ContentText = ({ title, text }) => {
+  if (Array.isArray(text)) {
+    return (
+      <div>
+        <strong>{title}</strong>
+        <ul>
+          {text.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return (
+      <p>
+        <strong>{title}</strong>
+        <span> &#8211; </span>
+        {text}
+      </p>
+    );
+  }
+};
 
 export default ContentText;
