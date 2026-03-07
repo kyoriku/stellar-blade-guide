c48a597db5bc4f64dc10eb4222642a2d01a3a2a8
kyoriku <austiingraham@gmail.com>
2026-03-06 19:19:47 -0500

    test client watch path

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -52,13 +52,10 @@ function App() {
           {/* Collectibles */}
           <Route path="collectibles" element={<CollectiblesIndexPage />} />
           <Route path="collectibles/:typeName" element={<CollectibleTypePage />} />
-
           <Route path="upgrades" element={<UpgradesIndexPage />} />
           <Route path="upgrades/:typeName" element={<CollectibleTypePage />} />
-
           <Route path="materials" element={<MaterialsIndexPage />} />
           <Route path="materials/:typeName" element={<CollectibleTypePage />} />
-
           <Route path="cosmetics" element={<CosmeticsIndexPage />} />
           <Route path="cosmetics/:typeName" element={<CollectibleTypePage />} />
 
