277d0cd75cbb570adb13560edf3d24d69927a614
kyoriku <austiingraham@gmail.com>
2026-06-13 16:05:34 -0400

    add NieR and Nikke DLC guides

--- client/src/layouts/RootLayout.tsx
+++ client/src/layouts/RootLayout.tsx
@@ -1,12 +1,19 @@
+// import { useEffect } from 'react'
 import { Outlet } from 'react-router-dom'
 import Navbar from '../components/Navbar'
 import ScrollToTop from '../components/ScrollToTop'
 import Footer from '../components/Footer'
 
 function RootLayout() {
+  // useEffect(() => {
+  //   if ('scrollRestoration' in window.history) {
+  //     window.history.scrollRestoration = 'manual';
+  //   }
+  // }, []);
 
   return (
     <div className="flex flex-col min-h-screen bg-primary">
+      {/* <div className="flex flex-col min-h-dvh bg-primary"> */}
       <ScrollToTop />
       <Navbar />
       <div className="flex-1">
